5fe218bcffb89bb5a3eddfa52b6ea40faef0cd6c
vagrant <vagrant@localhost.localdomain>
2020-09-29 11:58:17 +0000

    Update migrate_bookmarks

--- db/migrate/20200921065042_create_bookmarks.rb
+++ db/migrate/20200921065042_create_bookmarks.rb
@@ -1,13 +1,13 @@
 class CreateBookmarks < ActiveRecord::Migration[5.2]
-  def change
-    create_table :bookmarks do |t|
+  # def change
+  #   create_table :bookmarks do |t|
 
-      # t.references :user, null: false, foreign_key: true
-      # t.references :restaurant, null: false, foreign_key: true
-      t.integer :user_id
-      t.integer :restaurant_id
+  #     # t.references :user, null: false, foreign_key: true
+  #     # t.references :restaurant, null: false, foreign_key: true
+  #     t.integer :user_id
+  #     t.integer :restaurant_id
 
-      t.timestamps
-    end
-  end
+  #     t.timestamps
+  #   end
+  # end
 end
