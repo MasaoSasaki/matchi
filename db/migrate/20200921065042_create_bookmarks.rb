a46284e326355b3314d7ba6e9563c58d5c74db44
vagrant <vagrant@localhost.localdomain>
2020-09-30 08:00:15 +0000

    Fix conflict

--- db/migrate/20200921065042_create_bookmarks.rb
+++ db/migrate/20200921065042_create_bookmarks.rb
@@ -2,12 +2,10 @@ class CreateBookmarks < ActiveRecord::Migration[5.2]
   # def change
   #   create_table :bookmarks do |t|
 
-  #     # t.references :user, null: false, foreign_key: true
-  #     # t.references :restaurant, null: false, foreign_key: true
-  #     t.integer :user_id
-  #     t.integer :restaurant_id
+      t.references :user, null: false, foreign_key: true
+      t.references :restaurant, null: false, foreign_key: true
 
-  #     t.timestamps
-  #   end
-  # end
+      t.timestamps
+    end
+  end
 end
