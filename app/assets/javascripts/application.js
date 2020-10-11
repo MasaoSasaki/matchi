6224fade132ea5de92ab501e9260c7190ba92ebd
vagrant <vagrant@localhost.localdomain>
2020-10-11 03:57:21 +0000

    Update application.js remobe top_scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,135 +17,116 @@
 //= require_tree .
 
 // 地図情報取得
-addEventListener('DOMContentLoaded', function() {
-  if (document.getElementById("restaurants_json") != null) {
-    // homes/top.html.erb(:1 JSONタグ)が読み込まれたら現在地を取得開始
-    $("#restaurants_json").ready(function() {
-      $("#loading").append("(読み込み中)");
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          const CurrentLat = position.coords.latitude;
-          const CurrentLng = position.coords.longitude;
+if (document.getElementById("restaurants_json") != null) {
+  // homes/top.html.erb(:1 JSONタグ)が読み込まれたら現在地を取得開始
+  $("#restaurants_json").ready(function() {
+    $("#loading").append("(読み込み中)");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        const CurrentLat = position.coords.latitude;
+        const CurrentLng = position.coords.longitude;
 
-          //現在地を地図上に表示
-          $(function() {
-            map = new google.maps.Map(document.getElementById("map"), {
-              center: {lat: CurrentLat, lng: CurrentLng},
-              zoom: 13
-            });
-            marker = new google.maps.Marker({
-              position: new google.maps.LatLng(CurrentLat, CurrentLng),
-              map: map
-            });
-            infoWindow = new google.maps.InfoWindow({
-              content: "現在地付近"
-            });
-            marker.addListener("click", function(){
-              infoWindow.open(map, marker);
-            });
-
-            // レストラン情報を地図上に表示
-            const restaurants = gon.restaurants;
-            $(function(){
-              for (let i = 0; i < restaurants.length; i++) {
-                let address = restaurants[i].prefecture + restaurants[i].city + restaurants[i].street;
-                const geocoder = new google.maps.Geocoder();
-                geocoder.geocode(
-                  {"address": address},
-                  function(result, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                      let lat = result[0].geometry.location.lat();
-                      let lng = result[0].geometry.location.lng();
-                      marker[i] = new google.maps.Marker({
-                        position: new google.maps.LatLng(lat, lng),
-                        map: map
-                      });
-                      i = i + 1;
-                      let link = "https://matchi-gourmet.com/restaurants/"+i;
-                      i = i - 1;
-                      infoWindow[i] = new google.maps.InfoWindow(
-                        {
-                          content: restaurants[i].name + '<br><a href='+link+'>詳細</a>',
-                        }
-                      );
-                      marker[i].addListener("click", function(){
-                        infoWindow[i].open(map, marker[i]);
-                      });
-                    } else {
-                      $(".location-result" + ".error").removeClass("hidden");
-                    }
-                  }
-                );
-              }
-            });
+        //現在地を地図上に表示
+        $(function() {
+          map = new google.maps.Map(document.getElementById("map"), {
+            center: {lat: CurrentLat, lng: CurrentLng},
+            zoom: 13
           });
-          $("#loading").remove();
-        });
-      } else {
-        $(".location-result" + ".false").removeClass("hidden");
-        // レストラン情報を地図上に表示
-        const restaurants = gon.restaurants;
-        $(function(){
-          for (let i = 0; i < restaurants.length; i++) {
-            let address = restaurants[i].prefecture + restaurants[i].city + restaurants[i].street;
-            const geocoder = new google.maps.Geocoder();
-            geocoder.geocode(
-              {"address": address},
-              function(result, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                  var lat = result[0].geometry.location.lat();
-                  var lng = result[0].geometry.location.lng();
-                  marker[i] = new google.maps.Marker({
-                    position: new google.maps.LatLng(lat, lng),
-                    map: map
-                  });
-                  i = i + 1;
-                  var link = "https://matchi-gourmet.com/restaurants/"+i;
-                  i = i - 1;
-                  infoWindow[i] = new google.maps.InfoWindow(
-                    {
-                      content: restaurants[i].name + '<br><a href='+link+'>詳細</a>',
-                    }
-                  );
-                  marker[i].addListener("click", function(){
-                    infoWindow[i].open(map, marker[i]);
-                  });
-                } else {
-                  $(".location-result" + ".error").removeClass("hedden");
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(CurrentLat, CurrentLng),
+            map: map
+          });
+          infoWindow = new google.maps.InfoWindow({
+            content: "現在地付近"
+          });
+          marker.addListener("click", function(){
+            infoWindow.open(map, marker);
+          });
+
+          // レストラン情報を地図上に表示
+          const restaurants = gon.restaurants;
+          $(function(){
+            for (let i = 0; i < restaurants.length; i++) {
+              let address = restaurants[i].prefecture + restaurants[i].city + restaurants[i].street;
+              const geocoder = new google.maps.Geocoder();
+              geocoder.geocode(
+                {"address": address},
+                function(result, status) {
+                  if (status == google.maps.GeocoderStatus.OK) {
+                    let lat = result[0].geometry.location.lat();
+                    let lng = result[0].geometry.location.lng();
+                    marker[i] = new google.maps.Marker({
+                      position: new google.maps.LatLng(lat, lng),
+                      map: map
+                    });
+                    i = i + 1;
+                    let link = "https://matchi-gourmet.com/restaurants/"+i;
+                    i = i - 1;
+                    infoWindow[i] = new google.maps.InfoWindow(
+                      {
+                        content: restaurants[i].name + '<br><a href='+link+'>詳細</a>',
+                      }
+                    );
+                    marker[i].addListener("click", function(){
+                      infoWindow[i].open(map, marker[i]);
+                    });
+                  } else {
+                    $(".location-result" + ".error").removeClass("hidden");
+                  }
                 }
-              }
-            );
-          }
+              );
+            }
+          });
         });
         $("#loading").remove();
-      }
-    });
-
-    // geolocationが使える場合、クリックで地図を表示
-    $("#get-position").on("click", function() {
-      if (navigator.geolocation) {
-        $(".location-result" + ".true").removeClass("hidden");
-      }
-    });
-  }
-});
-
-// ページトップにスクロール
-const scrollIcon = document.getElementById("move-head--icon");
-addEventListener('scroll', function() {
-  if (pageYOffset == 0 ) {
-    scrollIcon.classList.add("hidden");
-  } else {
-    scrollIcon.classList.remove("hidden");
-  }
-})
-scrollIcon.addEventListener('click', function() {
-  scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
+      });
+    } else {
+      $(".location-result" + ".false").removeClass("hidden");
+      // レストラン情報を地図上に表示
+      const restaurants = gon.restaurants;
+      $(function(){
+        for (let i = 0; i < restaurants.length; i++) {
+          let address = restaurants[i].prefecture + restaurants[i].city + restaurants[i].street;
+          const geocoder = new google.maps.Geocoder();
+          geocoder.geocode(
+            {"address": address},
+            function(result, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                var lat = result[0].geometry.location.lat();
+                var lng = result[0].geometry.location.lng();
+                marker[i] = new google.maps.Marker({
+                  position: new google.maps.LatLng(lat, lng),
+                  map: map
+                });
+                i = i + 1;
+                var link = "https://matchi-gourmet.com/restaurants/"+i;
+                i = i - 1;
+                infoWindow[i] = new google.maps.InfoWindow(
+                  {
+                    content: restaurants[i].name + '<br><a href='+link+'>詳細</a>',
+                  }
+                );
+                marker[i].addListener("click", function(){
+                  infoWindow[i].open(map, marker[i]);
+                });
+              } else {
+                $(".location-result" + ".error").removeClass("hedden");
+              }
+            }
+          );
+        }
+      });
+      $("#loading").remove();
+    }
   });
-});
+
+  // geolocationが使える場合、クリックで地図を表示
+  $("#get-position").on("click", function() {
+    if (navigator.geolocation) {
+      $(".location-result" + ".true").removeClass("hidden");
+    }
+  });
+}
 
 // ハンバーガーメニュー
 $(function() {
