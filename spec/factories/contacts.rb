d781ff0d15a36c59f4754a8c6ead8c905fa05ab6
vagrant <vagrant@localhost.localdomain>
2020-10-12 06:39:23 +0000

    Add contacts_factory_bot Update models/contact_spec.rb

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :contact do
+    email {"a@a"}
+    message {"test"}
+  end
+end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'バリデーションのテスト' do
+    context ':contactが保存できる' do
+      it ':emailに@がなければ保存できない' do
+        contact = build(:contact, email: 'aa')
+        expect(contact).to be_invalid
+      end
+      it ':emailの値が空白の場合は保存できない' do
+        contact = build(:contact, email: nil)
+        expect(contact).to be_invalid
+      end
+      it ':messageの値が空白の場合は保存できない' do
+        contact = build(:contact, message: nil)
+        expect(contact).to be_invalid
+      end
+    end
+  end
+
 end
