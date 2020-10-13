0cb876dc2f04b1e56a10c71a282e6718e8e24843
vagrant <vagrant@localhost.localdomain>
2020-10-13 05:34:27 +0000

    Update admin_model add validates

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,13 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーションチェック
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  with_options length: { maximum: 255 } do
+    validates :email
+    validates :password
+  end
+
 end
