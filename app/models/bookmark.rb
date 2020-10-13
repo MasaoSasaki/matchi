ddb6d77aa2d4949d211f9e15fbb86ac094d06c84
vagrant <vagrant@localhost.localdomain>
2020-10-13 05:46:58 +0000

    Update bookmar_models add validates

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,12 @@
 class Bookmark < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :restaurant
+
+  # バリデーション
+  with_options numericality: { only_integer: true } do
+    validates :user_id
+    validates :restaurant_id
+  end
+
 end
