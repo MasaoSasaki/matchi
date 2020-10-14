ac7538f373f23139a78e098093edb5f63aa70270
vagrant <vagrant@localhost.localdomain>
2020-10-14 04:53:57 +0000

    Add users_factory update user_spec.rb

---
+++ spec/factories/users.rb
@@ -0,0 +1,24 @@
+FactoryBot.define do
+
+  factory :user do
+    id { 1 }
+    email { "a@a.a" }
+    password { "000000" }
+    name_family { "田中" }
+    name_first { "太郎" }
+    name_family_kana { "たなか" }
+    name_first_kana { "たろう" }
+    profile { "" }
+    profile_image_id { "" }
+    twitter { "" }
+    facebook { "" }
+    instagram { "" }
+    phone_number { "00000000000" }
+    email_sub { "b@b.b" }
+    birth_year { 1900 }
+    birth_month { 1 }
+    birth_day { 1 }
+    user_status { 0 }
+  end
+
+end
