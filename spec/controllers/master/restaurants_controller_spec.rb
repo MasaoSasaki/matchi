d9abf5175feabc16feff4fc5cddfd1f6005e4f91
vagrant <vagrant@localhost.localdomain>
2020-10-16 02:54:46 +0000

    Remove controller_specs

--- spec/controllers/master/restaurants_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Master::RestaurantsController, type: :controller do
-
-end
--- spec/controllers/owner/menu_tags_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Owner::MenuTagsController, type: :controller do
-
-end
