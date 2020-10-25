8a74f620e31e35badd5da099abc7a834ace3b322
vagrant <vagrant@localhost.localdomain>
2020-10-25 05:10:57 +0000

    Add system/master/restaurants_spec.rb

---
+++ spec/system/master/restaurants_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Restaurants' do
+  before do
+    admin = create(:admin)
+    sign_in admin
+  end
+  describe 'indexページのテスト' do
+    it '店舗一覧一覧画面が表示される' do
+      visit master_restaurants_path
+      expect(page).to have_content '店舗一覧'
+    end
+  end
+end
